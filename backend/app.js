77d193c3fc5689cc37696fd57bb9c54790cf4b59
MPrav143 <praveen17082005@gmail.com>
2026-02-14 11:03:39 +0530

    14th Commit

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [
-    'https://kcedhruva-orpin.vercel.app'
+    'https://kcedhruva-orpin.vercel.app',
 ];
 
 if (process.env.CLIENT_URL) {
